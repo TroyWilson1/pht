187541bbc0fc0dd8a8772faef1f162ee24cfe61b
Troy Wilson <troy@merchantguy.com>
2018-02-27 09:21:15 -0600

    Received new code from Code Review

--- pht.py
+++ pht.py
@@ -1,54 +1,67 @@
 #!/usr/bin/python
-# PHT - Personal Hash Tracker
-#
-import json
+
 import os
-
-hashjson_global = "/Users/troywilson/testing/pht/hash.json"
-
-data = []
-if os.stat(hashjson_global).st_size != 0 :
-    file = open(hashjson_global, 'r')
-    data = json.load(file)
-   # print(data)
-
-choice = raw_input("What do you want to do? \n a)Add a new IPFS hash\n s)Seach stored hashes\n d)Delete stored hash\n >>")
-
-if choice == 'a':
-    # Add a new hash.
-    description = raw_input('Enter hash description: ')
-    new_hash_val = raw_input('Enter IPFS hash: ')
-    new_url_val = raw_input('Enter URL: ')
-    entry = {new_hash_val: {'description': description, 'url': new_url_val}}
-
-    # search existing hash listings here
-    if new_hash_val not in data['hashlist']:
-    # append JSON file with new entry
-        data['hashlist'].update(entry) #must do update since it's a dictionary
-        with open(hashjson_global, 'w') as file:
-            json.dump(data, file, sort_keys = True, indent = 4, ensure_ascii = False)
-        print('IPFS Hash Added.')
-        pass
-    else:
-        print('Hash exist!')
-        
-elif choice == 's':
-    # Search the current desciptions.
-    searchTerm = raw_input('Enter search term: ')
-    with open(hashjson_global, 'r') as file:
-        data = json.load(file)
-        hashlist = data['hashlist']
-    # build dictionary map and search for description value
-    d = {v['description']: h for h, v in hashlist.items()}
-    print d.get(searchTerm, 'Not Found')
+import json
 
 
-elif choice == 'd':
-    # Search the current descriptions and delete entry.
-    del_hash = raw_input('Hash to delete: ')
-    with open(hashjson_global, 'r') as file:
-        data = json.load(file)
-    del data['hashlist'][del_hash]
-    with open('hashjson', 'w') as file:
-            json.dump(data, file, sort_keys = True, indent = 4, ensure_ascii = False)
-    print ('Hash removed')
+class PersonalHashTracker(dict):
+    def __init__(self, filename):
+        self.filename = filename
+        if os.path.isfile(filename):
+            with open(filename) as f:
+                # use super here to avoid unnecessary write
+                super(PersonalHashTracker, self).update(json.load(f) or {})
+
+    def __setitem__(self, key, value):
+        super(PersonalHashTracker, self).__setitem__(key, value)
+        with open(self.filename, "w") as f:
+            json.dump(self, f)
+
+    def __delitem__(self, key):
+        super(PersonalHashTracker, self).__delitem__(key)
+        with open(self.filename, "w") as f:
+            json.dump(self, f)
+
+    def update(self, d, **kwargs):
+        super(PersonalHashTracker, self).update(d, **kwargs)
+        with open(self.filename, "w") as f:
+            json.dump(self, f)
+
+
+MENU = """What do you want to do?
+a)Add a new IPFS hash
+s)Seach stored ha:shes
+d)Delete stored hash
+q)Quit
+>>"""
+
+if __name__ == "__main__":
+    hash_store = PersonalHashTracker("/Users/troywilson/testing/pht/hash.json")
+    if not hash_store:
+        hash_store['hashlist'] = {}
+
+    while True:
+        choice = raw_input(MENU)
+        if choice == 'a':
+            # Add a new hash.
+            description = raw_input('Enter hash description: ')
+            new_hash_val = raw_input('Enter IPFS hash: ')
+            new_url_val = raw_input('Enter URL: ')
+
+            if new_hash_val not in data['hashlist']:
+                hash_store['hashlist'][new_hash_val] = {'description': description, 'url': new_url_val}
+            else:
+                print 'Hash exist!'
+        elif choice == 's':
+            # Search the current desciptions.
+            search_term = raw_input('Enter search term: ')
+            descriptions = {v['description']: h for h, v in hash_store['hashlist'].items()}
+            print(descriptions.get(search_term, 'Not Found'))
+        elif choice == 'd':
+            # Search the current descriptions and delete entry.
+            del_hash = raw_input('Hash to delete: ')
+            del data['hashlist'][del_hash]
+            print 'Hash removed'
+        else:
+            print 'Exiting'
+            break
